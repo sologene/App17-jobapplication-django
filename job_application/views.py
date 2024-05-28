fe4e67dcb060ac51c467e36ad67aea31be57493a
Sologene <basil.s.maliyil@gmail.com>
2024-05-28 13:19:04 +0800

    Added front end

--- job_application/views.py
+++ job_application/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
-
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 
@@ -14,5 +16,19 @@ def index(request):
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
 
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+
+            message_body = f"Hello, {first_name}\nA new job application was submitted, Thank you!!\n" \
+                           f"Here is your data:\n{first_name}\n{last_name}\n{email}\n{date}\n{occupation}"
+            email_message = EmailMessage("Form submission confirmation", message_body, to=[email])
+            email_message.send()
+
+
+            messages.success(request,"Form submitted successfully!!!")
 
     return render(request, "index.html")
+
+
+def about(request):
+    return render(request,"about.html")
